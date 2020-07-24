c6c11a2f0d680a2b2f97e2468f9fd0c980d81636
paulgureghian <paulgureghian@att.net>
2020-07-23 17:44:27 -0700

    Converted to CommonJS

--- node-dependencies/5-exercise-dependency/address-parser.js
+++ node-dependencies/5-exercise-dependency/address-parser.js
@@ -1,4 +1,4 @@
-export const parse = function parseOrder(order) {
+exports.parse = function parseOrder(order) {
   const match = order.match(/order:\s(?<order>\w+\s\w+).*address:\s(?<address>\w+\s\w+\s\w+).*payment info:\s(?<payment>\w+)/)
-  return match.groups;
+ return match.groups;
 }
