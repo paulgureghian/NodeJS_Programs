31157448c6f1cbd51fb6b8ac816e5f9f779bf19e
paulgureghian <paulgureghian@att.net>
2021-03-04 18:49:46 -0800

    Modified file

--- Intro_to_NodeJS /static/app.js
+++ Intro_to_NodeJS /static/app.js
@@ -27,15 +27,18 @@ const add = document.querySelector('#add')
 
 let socket = null
 const realtimeOrders = (category) => {
-    if (socket) socket.close()
-    socket = new WebSocket(` ${WS_API}/orders/${category} `)
+    if (socket === null) {
+        socket = new WebSocket(` ${WS_API}/orders/${category} `)
+    } else {
+        socket.send(JSON.stringify({cmd: 'update-category', payload: {category}}))
+    }
+
     socket.addEventListener('message', ({data}) => {
         try {
             const {id, total} = JSON.parse(data)
             const item = document.querySelector(` [data-id="${id}"] `)
             if (item === null) return
-            const span = item.querySelector('[slot="orders"]') ||
-                document.createElement('span')
+            const span = item.querySelector('[slot="orders"]') || document.createElement('span')
             span.slot = 'orders'
             span.textContent = total
             item.appendChild(span)
