20a14a2ebe02d2307b803f56cc631874e76811ed
paulgureghian <paulgureghian@att.net>
2021-03-04 19:55:09 -0800

    Modified file

--- Intro_to_NodeJS /mock-srv/routes/orders/index.js
+++ Intro_to_NodeJS /mock-srv/routes/orders/index.js
@@ -1,13 +1,35 @@
 'use strict'
 
 module.exports = async function (fastify, opts) {
+    function monitorMessages(socket) {
+        socket.on('message', (data) => {
+            try {
+                const {cmd, payload} = JSON.parse(data)
+                if (cmd === 'update-category') {
+                    sendCurrentOrders(payload.category, socket)
+                }
+            } catch (err) {
+                fastify.log.warn(
+                    'WebSocket Message (data: %o) Error: %s',
+                    data,
+                    err.message
+                )
+            }
+        })
+    }
+
+    function sendCurrentOrders(category, socket) {
+        for (const order of fastify.currentOrders(category)) {
+            socket.send(order)
+        }
+    }
+
     fastify.get(
         '/:category',
         {websocket: true},
         async ({socket}, request) => {
-            for (const order of fastify.currentOrders(request.params.category)) {
-                socket.send(order)
-            }
+            monitorMessages(socket)
+            sendCurrentOrders(request.params.category, socket)
             for await (const order of fastify.realtimeOrders()) {
                 if (socket.readyState >= socket.CLOSING) break
                 socket.send(order)
