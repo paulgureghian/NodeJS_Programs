1b466a1acc54e38c1abf1452037125555c992402
paulgureghian <paulgureghian@att.net>
2021-03-04 16:05:34 -0800

    Modified file

--- Intro_to_NodeJS /mock-srv/app.js
+++ Intro_to_NodeJS /mock-srv/app.js
@@ -3,17 +3,19 @@
 const path = require('path')
 const AutoLoad = require('fastify-autoload')
 const cors = require('fastify-cors')
+const websocket = require('fastify-websocket')
 
 module.exports = async function (fastify, opts) {
-  fastify.register(cors)
+    fastify.register(cors)
+    fastify.register(websocket)
 
-  fastify.register(AutoLoad, {
-    dir: path.join(__dirname, 'plugins'),
-    options: Object.assign({}, opts)
-  })
+    fastify.register(AutoLoad, {
+        dir: path.join(__dirname, 'plugins'),
+        options: Object.assign({}, opts)
+    })
 
-  fastify.register(AutoLoad, {
-    dir: path.join(__dirname, 'routes'),
-    options: Object.assign({}, opts)
-  })
+    fastify.register(AutoLoad, {
+        dir: path.join(__dirname, 'routes'),
+        options: Object.assign({}, opts)
+    })
 }
