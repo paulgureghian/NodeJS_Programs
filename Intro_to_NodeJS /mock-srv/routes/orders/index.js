160f3289b91ef55e7636113604506fa03ee443aa
paulgureghian <paulgureghian@att.net>
2021-03-04 18:20:09 -0800

    Added a web sockets module

---
+++ Intro_to_NodeJS /mock-srv/routes/orders/index.js
@@ -0,0 +1,17 @@
+'use strict'
+
+module.exports = async function (fastify, opts) {
+    fastify.get(
+        '/:category',
+        {websocket: true},
+        async ({socket}, request) => {
+            for (const order of fastify.currentOrders(request.params.category)) {
+                socket.send(order)
+            }
+            for await (const order of fastify.realtimeOrders()) {
+                if (socket.readyState >= socket.CLOSING) break
+                socket.send(order)
+            }
+        }
+    )
+}
